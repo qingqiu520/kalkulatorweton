7d727782a1f43989ebe9300d50663d1ecc4dabc1
alele <3479721800@qq.com>
2026-09-16 19:31:30 +0800

    Add GA4 tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="id">
       <body className="flex min-h-screen flex-col">
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MMRB0J2L8T"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MMRB0J2L8T');`,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
